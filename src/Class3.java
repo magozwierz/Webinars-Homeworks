148b620a00dfac330cad2ef62e121735d98519b9
magozwierz <social.zwierzynska@gmail.com>
2021-11-17 11:23:38 +0100

    classes1-3 commit

---
+++ src/Class3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Class3 {
+    public static void main(String[] args) {
+        //to jest komentarz
+/*   wielolinijkowy
+   komentarz
+       jest
+               tutaj
+
+ */
+
+        int wiekUzywkonika = 18; //deklaracja z i
+        System.out.println(wiekUzywkonika);
+
+        //trzy sposoby na inkrementację o1
+        wiekUzywkonika++;
+        wiekUzywkonika = wiekUzywkonika + 1;
+        wiekUzywkonika +=1;
+        System.out.println(wiekUzywkonika);
+
+        //dekrementacja:
+        wiekUzywkonika--;
+        System.out.println(wiekUzywkonika);
+
+        int a = 5, b = 6, c = 7; //deklaracja + inicjalizacja wielu zmiennych
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
