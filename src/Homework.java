a4568f399511cd258b8025e0986ee18e2c8b6a8e
magozwierz <social.zwierzynska@gmail.com>
2021-11-17 11:08:50 +0100

    Homework1 commit2

---
+++ src/Homework.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+
+
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+        System.out.println("    *    ");
+
+    }
+}
